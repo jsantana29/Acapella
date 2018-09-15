d260ba200946d94b785d38900b691236b3bcb7d9
jsantana29 <santanarijo29@gmail.com>
2018-09-15 02:16:41 -0400

    Player movement
    Coded the player movement to move with WASD

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private float speed = 5;    //brackevs source vid
+    private PlayerMove motor;
+    //Input.GetButton("Horizontal"); ARTIFACT
+
+	// Use this for initialization
+	void Start () {
+        motor = GetComponent<PlayerMove>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //return a percentage of how hard you are pushing a stick
+        float xMovment = Input.GetAxisRaw("Horizontal");
+        float yMovment = Input.GetAxisRaw("Vertical");
+
+        //make a velocity vector that "pushes" the character
+        Vector3 moveSide = transform.right * xMovment;
+        Vector3 moveFront = transform.forward * yMovment;
+
+        //the speed of the character
+        Vector3 velocity = (moveSide + moveFront).normalized * speed;
+
+        //move 
+       motor.Move(velocity);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMove : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Vector3 finalVelocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        PerformUpdate();
+	}
+
+    public void Move(Vector3 velocity)
+    {
+        finalVelocity = velocity;
+    }
+
+    void PerformUpdate()
+    {
+        rb.MovePosition(rb.position + finalVelocity * Time.fixedDeltaTime);
+    }
+}
