35d9a70f3e7284b675afdd4d7b86eda7ca791139
jsantana29 <santanarijo29@gmail.com>
2018-09-15 18:12:13 -0400

    Added shooting and jumping

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -6,14 +6,21 @@ public class BulletController : MonoBehaviour {
 
     
     private Material material;
+    private Rigidbody2D rb;
+    private Vector3 bulletVelocity;
+
+
+    [SerializeField]
+    private int bulletSpeed = 5;
 
 	// Use this for initialization
 	void Start () {
         //material.color = Color.red;
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //rb.velocity = transform.forward * 6;
 	}
 }
---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour {
+
+    private Rigidbody rb;
+    
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+    
+
+
+}
